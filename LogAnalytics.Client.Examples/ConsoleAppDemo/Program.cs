be584a041a7aef66a4228f856ec964d5b69998ca
Jan Skala <j.skala@hotmail.com>
2021-02-08 22:55:43 +0100

    Scaffold projects.

---
+++ LogAnalytics.Client.Examples/ConsoleAppDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
