0169d523d84402aa9878d6c63967f268252bf662
Tobias Zimmergren <tobias@zimmergren.net>
2021-02-08 12:35:14 +0100

    Adding support for specifying ResourceID and TimeGeneratedField parameters

---
+++ LogAnalytics.Client/LogAnalytics.Client.IntegrationTests/Tests/Client/TestEntities/CustomTimeGeneratedTestEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogAnalytics.Client.IntegrationTests.Tests.Client.TestEntities
+{
+    public class CustomTimeGeneratedTestEntity
+    {
+        public string Criticality { get; set; }
+        public string SystemSource { get; set; }
+        public string Message { get; set; }
+        public int Priority { get; set; }
+        public DateTime CustomDateTime { get; set; }
+    }
+}
