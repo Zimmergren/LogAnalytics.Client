1611b4901af5b9c5e566839637c9270859cde7f2
Jan Skala <j.skala@hotmail.com>
2021-02-08 23:11:41 +0100

    Add Console sample.

--- LogAnalytics.Client.Examples/ConsoleAppDemo/Program.cs
+++ LogAnalytics.Client.Examples/ConsoleAppDemo/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿
+using System;
+using System.Threading.Tasks;
+
+using LogAnalytics.Client;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleAppDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder() // load configuration
+                .AddEnvironmentVariables() // from environment variables
+                .AddCommandLine(args) // or command line arguments
+                .AddJsonFile("appsettings.json") // or json file
+                .AddUserSecrets<Program>() // or user secrets
+                .Build();
+
+            var services = new ServiceCollection()
+                .AddLogAnalyticsClient(configuration.GetSection("LogAnalytics"))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<LogAnalyticsClient>();
+
+            await logger.SendLogEntry(new TestEntity
+            {
+                Category = "Foo",
+                TestString = $"String Test",
+                TestBoolean = true,
+                TestDateTime = DateTime.UtcNow,
+                TestDouble = 2.1,
+                TestGuid = Guid.NewGuid()
+            }, "demolog");
         }
     }
 }
---
+++ LogAnalytics.Client.Examples/ConsoleAppDemo/TestEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppDemo
+{
+    public class TestEntity
+    {
+        public string Category { get; set; }
+        public string TestString { get; set; }
+        public bool TestBoolean { get; set; }
+        public DateTime TestDateTime { get; set; }
+        public double TestDouble { get; set; }
+        public Guid TestGuid { get; set; }
+    }
+}
