3e771f54ab7423ec5cdc93facb7e2c0e085e4fdf
Tobias Zimmergren <tobias@zimmergren.net>
2021-07-08 10:54:48 +0200

    Initial Azure Sovereign Cloud support. Integration tests for default configuration works

---
+++ LogAnalytics.Client/LogAnalytics.Client/Configuration/LogAnalyticsClientOptions.cs
@@ -0,0 +1,26 @@
+﻿using LogAnalytics.Client.Configuration;
+
+namespace LogAnalytics.Client
+{
+    /// <summary>
+    /// Log Analytics Client Options.
+    /// Defines settings for the Log Analytics Client.
+    /// </summary>
+    public class LogAnalyticsClientOptions
+    {
+        /// <summary>
+        /// Gets or sets the Workspace ID.
+        /// </summary>
+        public string WorkspaceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Shared Key.
+        /// </summary>
+        public string SharedKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure Sovereign Cloud.
+        /// </summary>
+        public AzureSovereignCloud AzureSovereignCloud { get;set; }
+    }
+}
